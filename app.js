2274c4bad070468e0eeb2881006e82d42b947128
copiyodiazf <copiyo_diaz@hotmail.com>
2021-09-22 22:50:28 -0300

    subiendo proyecto entero

---
+++ app.js
@@ -0,0 +1,22 @@
+const express= require ('express');
+const process = require('process');
+
+const app= express();
+const path=require('path');
+
+app.use(express.static(path.resolve(__dirname,'./public')));
+
+app.listen(3000, () => {
+    console.log("SERVER corriendo en http://localhost:3000");
+  });
+  
+app.get('/login',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/login.html'))
+})
+app.get('/register',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/register.html'))
+})
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/home.html'))
+})
